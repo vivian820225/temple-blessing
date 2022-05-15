8be7894b3106f0023902423afa262652ef1dd9d7
Vivian.C <vivian820225@gmail.com>
2022-05-16 00:39:11 +0800

    [Add] get record

--- src/views/record.vue
+++ src/views/record.vue
@@ -2,7 +2,7 @@
   <div class="record-layer linear-bg-color">
     <div class="flex items-center justify-between md:mb-8 mb-4 select-none">
       <router-link to="/" class="cursor-pointer">
-        <img class="logo-w" src="@/assets/images/logo-w.png" alt="楊梅玉玄宮" />
+        <img class="logo-w" src="@/assets/images/logo-w.png" alt="楊梅開基玉玄宮" />
       </router-link>
       <div class="flex items-center justify-end">
         <router-link to="/blessing" class="btn-solid-w whitespace-nowrap">
@@ -18,15 +18,22 @@
       </div>
       <div class="shadow-box">
         <Table :data="tableData" height="400" class="record-table" border stripe>
-          <TableColumn prop="date" label="點燈時間" align="center"> </TableColumn>
-          <TableColumn prop="type" label="燈種" align="center"> </TableColumn>
+          <TableColumn prop="created_at" label="點燈日期" align="center">
+            <template slot-scope="scope">
+              <span>{{ scope.row.created_at.split('T')[0]}}</span>
+            </template>
+          </TableColumn>
+          <TableColumn prop="type_name" label="燈種" align="center"></TableColumn>
           <TableColumn label="金額" align="center">
             <template slot-scope="scope">
               <span>${{ scope.row.price }}</span>
             </template>
           </TableColumn>
-          <TableColumn prop="account" label="匯款帳號後五碼" align="center"> </TableColumn>
-          <TableColumn prop="state" label="狀態" align="center"> </TableColumn>
+          <TableColumn prop="payment_state" label="狀態" align="center">
+            <template slot-scope="scope">
+              <span>{{ getPaymentStatus(scope.row.payment_state) }}</span>
+            </template>
+          </TableColumn>
           <TableColumn label="詳情" align="center" width="120" fixed="right">
             <template slot-scope="scope">
               <button
@@ -51,64 +58,52 @@
 <script>
 import { Table, TableColumn, Pagination, Dialog } from 'element-ui'
 import OrderDetail from '@/components/OrderDetail.vue'
+// import Tools from '@/utils/tools.js'
+import Publish from '@/api/publish.js'
+
 export default {
   name: 'Record',
   components: { Table, TableColumn, Pagination, Dialog, OrderDetail },
   data () {
     return {
       pageTotal: 1,
-      tableData: [
-        {
-          date: '2021/11/09 22:40',
-          name: '王大明',
-          type: '財寶神衣燈',
-          price: 3600,
-          phone: '0987383383',
-          birthday: '2000-01-12',
-          address: ' 陶珠隱園市陶珠隱園路陶珠隱園巷87號87樓',
-          account: 56753,
-          state: '已付款',
-          payment: 'transform',
-          checkedDate: '2021/11/10 08:59'
-        },
-        {
-          date: '2021/12/19 12:40',
-          name: '巧克力',
-          type: '光明燈',
-          price: 500,
-          phone: '0987383383',
-          birthday: '1999-07-28',
-          address: ' 陶珠隱園市陶珠隱園路陶珠隱園巷87號87樓',
-          account: 12753,
-          state: '尚未匯款',
-          payment: 'transform',
-          checkedDate: '2021/12/20 08:59'
-        },
-        {
-          date: '2021/12/19 12:40',
-          name: '巧克力',
-          type: '光明燈',
-          price: 500,
-          phone: '0987383383',
-          birthday: '1999-07-28',
-          address: ' 陶珠隱園市陶珠隱園路陶珠隱園巷87號87樓',
-          account: 12753,
-          state: '已取消',
-          payment: 'transform',
-          checkedDate: '2021/12/20 08:59'
-        }
-      ],
+      tableData: [],
       dialogVisible: false,
       order: {}
     }
   },
+  computed: {
+    phone () {
+      return this.$store.state.userInfo.phoneNumber
+    }
+  },
+  created () {
+    this.getRecord()
+  },
   methods: {
+    async getRecord () {
+      const res = await Publish.fetchCandleRecord(this.phone)
+      console.log(res)
+      this.tableData = res
+    },
+    getPaymentStatus (status) {
+      switch (status) {
+        case 'paid':
+          return '已付款'
+        case 'unpaid':
+          return '尚未付款'
+        case 'unverified':
+          return '審核中'
+        default:
+          break
+      }
+    },
     openDetail (order) {
       this.order = order
       this.dialogVisible = true
     },
     logout () {
-      localStorage.removeItem('access_token')
+      this.$store.dispatch('logout')
       this.$router.push({ path: '/login' })
     }
   }
